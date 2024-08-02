6ca40831e7e676e574e506b60bb8139e86a90ab5
Elioacr <elio.cr.cl@gmail.com>
2024-08-02 15:54:14 -0300

    Solución de errores

--- ProyectoGrupo/src/main/resources/static/js/voluntario.js
+++ ProyectoGrupo/src/main/resources/static/js/voluntario.js
@@ -1,4 +1,5 @@
-function getCategoryFromPath() {
+// Filtro eventos por categoria
+function obtenerCategoriaDeRuta() {
     // Obtiene la ruta actual
     const path = window.location.pathname;
 
@@ -9,21 +10,84 @@ function getCategoryFromPath() {
     return match ? decodeURIComponent(match[1]) : null;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const select = document.getElementById('selectFiltro');
-    const category = getCategoryFromPath();
+// Filtro eventos por organizacion
+function obtenerOrganizacionDeRuta() {
+    // Obtiene la ruta actual
+    const path = window.location.pathname;
 
-    // Establece el valor seleccionado del select
-    if (category != null) {
-        select.value = category;
-    } else {
-        select.value = 'Todos';
-    }
-});
-document.getElementById('selectFiltro').addEventListener('change', function() {
-	if(this.value === 'Todos') {
-		window.location.href = "/voluntario";
+	// Extrae la organizacion de la ruta, suponiendo que la ruta sigue el formato '/eventos/filtrarOrganizacion/{}'
+	const match = path.match(/\/eventos\/filtrarOrganizacion\/(.+)/);
+
+    // Retorna la categoría decodificada o null si no hay coincidencia
+    return match ? decodeURIComponent(match[1]) : null;
+}
+
+function obtenerOrganizacionCategoriaDeRuta() {
+	// Obtiene la ruta actual
+    const path = window.location.pathname;
+
+	// Extrae la organización y la categoría de la ruta, suponiendo que la ruta sigue el formato '/eventos/filtrar/{organizacion}/{categoria}'
+    const match = path.match(/\/eventos\/filtrar\/([^\/]+)\/([^\/]+)/);
+
+    // Retorna un objeto con la organización y la categoría decodificadas, o null si no hay coincidencia
+    return match ? { organizacion: decodeURIComponent(match[1]), categoria: decodeURIComponent(match[2]) } : null;
+}
+document.addEventListener('DOMContentLoaded', function() {
+    const selectCat = document.getElementById('selectFiltroCategoria');
+    const selectOrg = document.getElementById('selectFiltroOrganizacion');
+    const filtros = obtenerOrganizacionCategoriaDeRuta();
+    if(filtros == null) {
+	    const category = obtenerCategoriaDeRuta();
+	    const organizacion = obtenerOrganizacionDeRuta();
+	    
+	    // Establece el valor seleccionado del select
+	    if (category != null) {
+	        selectCat.value = category;
+	    } else {
+	        selectCat.value = 'Todas las Categorias';
+	    }
+	    
+	    if (organizacion != null) {
+	        selectOrg.value = organizacion;
+	    } else {
+	        selectOrg.value = 'Todas las Organizaciones';
+	    }
 	} else {
-		window.location.href = '/eventos/filtrarCategoria/' + encodeURIComponent(this.value);
+		selectCat.value = filtros.categoria;
+    	selectOrg.value = filtros.organizacion;
+	}
+
+});
+document.getElementById('selectFiltroCategoria').addEventListener('change', function() {
+    const selectOrg = document.getElementById('selectFiltroOrganizacion');
+    if(selectOrg.value != 'Todas las Organizaciones') {
+		if(this.value === 'Todas las Categorias') {
+			window.location.href = '/eventos/filtrarOrganizacion/' + encodeURIComponent(selectOrg.value);
+		} else {
+			window.location.href = '/eventos/filtrar/' + encodeURIComponent(selectOrg.value) + '/' + encodeURIComponent(this.value);
+		}
+	} else {
+		if(this.value === 'Todas las Categorias') {
+			window.location.href = "/voluntario";
+		} else {
+			window.location.href = '/eventos/filtrarCategoria/' + encodeURIComponent(this.value);
+		}
+	}
+});
+
+document.getElementById('selectFiltroOrganizacion').addEventListener('change', function() {
+    const selectCat = document.getElementById('selectFiltroCategoria');
+    if(selectCat.value != 'Todas las Categorias') {
+		if(this.value === 'Todas las Organizaciones') {
+			window.location.href = "/eventos/filtrarCategoria/" + encodeURIComponent(selectCat.value);
+		} else {
+			window.location.href = '/eventos/filtrar/' + encodeURIComponent(this.value) + '/' + encodeURIComponent(selectCat.value);
+		}
+	} else {
+		if(this.value === 'Todas las Organizaciones') {
+			window.location.href = "/voluntario";
+		} else {
+			window.location.href = '/eventos/filtrarOrganizacion/' + encodeURIComponent(this.value);
+		}
 	}
 });
--- ProyectoGrupo/src/main/webapp/WEB-INF/organizacion.jsp
+++ ProyectoGrupo/src/main/webapp/WEB-INF/organizacion.jsp
@@ -1,19 +1,81 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-	<h1>Organización ${organizacion.nombreOrganizacion}</h1>
-	<ul>
-		<c:forEach var="evento" items="${eventos}">
-			<li><a href="/eventos/${evento.id}">${evento.nombre}</a> ${evento.descripcion} ${evento.ciudad} ${evento.categoria.categoria}</li>
-		</c:forEach>
-	</ul>
-	<a href="/eventos/nuevo">Crear Evento</a>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Organizacion</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+		<link rel="stylesheet" href="/css/organizacion.css">
+		 <link rel="icon" type="image/png" href="/images/minilogo.png"  type="image/x-icon">
+	</head>
+	<body>
+		 <nav class="navbar navbar-expand-lg bg-body-tertiary">
+	        <div class="container-fluid">
+	            <a class="navbar-brand" href="/inicio">
+	                <img src="/images/minilogo.png" alt="Bootstrap" width="50" height="">
+	            </a>
+	            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+	                <span class="navbar-toggler-icon"></span>
+	            </button>
+	            <div class="collapse navbar-collapse" id="navbarText">
+	                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	                    <li class="nav-item">
+	                        <a class="nav-link active" aria-current="page" href="/inicio">Inicio</a>
+	                    </li>
+	                    <li class="nav-item">
+	                        <a class="nav-link" href="/topvoluntarios">Top Voluntarios</a>
+	                    </li>
+	                </ul>
+	                <c:if test="${id_organizacion == null && id_usuario == null}">
+		                <a class="navbar-brand" href="/registro/organizacion">Quiero ser Empresa aliada</a>
+	                </c:if>
+	                <c:if test="${id_usuario != null || id_organizacion != null}">
+		                <form action="/logout" method="post">
+		                	<button type="submit" class="navbar-brand btn btn-link">Cerrar Sesión</button>
+						</form>
+	                </c:if>
+	            </div>
+	        </div>
+    	</nav>
+	
+	
+		<div class="container-custom mt-2">
+			<h1>Organizacion - Lista de Eventos Creados </h1>
+			<div class="scrollable-container">
+				<div class="row">
+					<c:forEach var="evento" items="${eventos}">
+						<div class="col-sm-6 mb-3">
+							<div class="card">
+								<div class="card-body">
+									<h4 class="card-title">${evento.nombre}</h4>
+									<p class="card-text">
+										<c:choose>
+                                        <c:when test="${fn:length(evento.descripcion) > 40}">
+                                            ${fn:substring(evento.descripcion, 0, 40)}...
+                                        </c:when>
+                                        <c:otherwise>
+                                            ${evento.descripcion}
+                                        </c:otherwise>
+                                    </c:choose>
+									 </p>
+									<p class="card-text"><strong>Ciudad:</strong>${evento.ciudad}</p>
+									<p class="card-text"><strong>Categoría:</strong>${evento.categoria.categoria}</p>
+									<a href="/eventos/${evento.id}" class="btn btn-primary">Ver Detalles</a>
+								</div>
+							</div>
+						</div>
+					</c:forEach>
+				
+				</div>
+			</div>
+			<a href="/eventos/nuevo" class="btn btn-primary mt-3">Crear Evento</a>
+		</div>
+		
+		
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+	</body>
+>>>>>>> d5d20b10290a45d98d489352ed2b2367162b34b6
 </html>
